7d23a9c9fe9218bb1f790c28be59b2515df268f2
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-07 15:31:15 +0100

    add movie selection prompt using inquirer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { config } from "dotenv";
 import axios from "axios";
+import inquirer from "inquirer";
 
 config();
 
+//console.log(process.env.ACCESS_TOKEN_AUTH)
+
+
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "things",
+        message: "Which movies do you want to see?",
+        choices: ["Currently on Theaters", "The most popular", "The top movies", "Upcoming movies"]
+    }
+])
+
+console.log(answer.things)
