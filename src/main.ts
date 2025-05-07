93780270c94a56c17ad9522f2e21c4d7548e7839
Ângel Cagiza <angelcagiza@gmail.com>
2025-05-07 16:03:47 +0100

    refactor: enhance movie selection logic and add type definitions

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,84 @@
 import { config } from "dotenv";
 import axios from "axios";
 import inquirer from "inquirer";
+import { abort } from "process";
+
+interface Movie{
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+interface ApiResponse {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
+}
 
 config();
 
 //console.log(process.env.ACCESS_TOKEN_AUTH)
-
+let url: string = ""
 
 const answer = await inquirer.prompt([
     {
         type: "list",
         name: "things",
         message: "Which movies do you want to see?",
-        choices: ["Currently on Theaters", "The most popular", "The top movies", "Upcoming movies"]
+        choices: ["Currently on Theaters", "The most popular movies", "The top movies", "Upcoming movies"]
     }
 ])
 
-console.log(answer.things)
+//console.log(answer.things)
+
+switch (answer.things){
+    case "Currently on Theaters":
+        url = "now_playing"
+        break
+
+    case "The most popular movies":
+        url = "popular"
+        break
+
+    case "The top movies":
+        url = "top_rated"
+        break
+
+    case "Upcoming movies":
+        url = "upcoming"
+        break
+
+    default:
+        console.error("Something Went Wrong :(")
+        abort()
+}
+
+
+const options = {
+    method: 'GET',
+    url: `https://api.themoviedb.org/3/movie/${url}?language=en-US&page=1`,
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ' + process.env.ACCESS_TOKEN_AUTH
+    }
+  };
+  
+  axios
+    .request<ApiResponse>(options)
+    .then(res => {
+        const movies: Movie[] = res.data.results;
+        movies.forEach(({ title }) => console.log(title));
+    })
+    .catch(err => console.error(err));
